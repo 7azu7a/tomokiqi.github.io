dafb1548c4f406d225140e94ef50c0188abda26d
tomokiqi <tomokiqi@gmail.com>
2021-06-18 21:16:32 +0900

    update blog list style

--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -1,7 +1,7 @@
 import { IBlog } from "../interfaces/blog";
 import dayjs from "dayjs";
-import Image from "next/image";
-import { Flex, Heading, Box, Text } from "@chakra-ui/react";
+// import Image from "next/image";
+import { Flex, Heading, Text, Image as ChakraImage } from "@chakra-ui/react";
 
 export const BlogList = ({ blogs }: { blogs: IBlog[] }) => {
   return (
@@ -10,27 +10,34 @@ export const BlogList = ({ blogs }: { blogs: IBlog[] }) => {
         <Flex
           direction="column"
           width="32rem"
-          height="16rem"
-          bgColor="white"
+          height="24rem"
+          bgImg="/washi.jpeg"
+          bgSize="100%"
+          bgRepeat="no-repeat"
           color="black"
           mx="2rem"
           key={content.id}
         >
-          <Image
+          <ChakraImage
             src={content.cover.url}
             alt="cover-image"
             width="100%"
-            height="100%"
+            height="70%"
             objectFit="cover"
           />
-          <Box flex="0.5" p="1rem">
-            <Heading fontSize="1rem" isTruncated>
+          <Flex
+            direction="column"
+            height="30%"
+            justifyContent="center"
+            p="1rem"
+          >
+            <Heading fontSize="1rem" isTruncated mb="0.5rem">
               {content.title}
             </Heading>
-            <Text>{`最終更新：${dayjs(content.updatedAt).format(
-              "YYYY/MM/DD"
-            )}`}</Text>
-          </Box>
+            <Text fontSize="0.707rem">{`最終更新：${dayjs(
+              content.updatedAt
+            ).format("YYYY/MM/DD")}`}</Text>
+          </Flex>
         </Flex>
       ))}
     </Flex>
