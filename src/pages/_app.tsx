e398c90896cde89ad97706af4f33a99db8338fa9
tomokiqi <tomokiqi@gmail.com>
2021-06-20 02:11:40 +0900

    fix font color on production

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import theme from "../theme";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
-      <Flex bgColor="heavyBlue" width="100vw" minHeight="100vh">
+      <Flex bgColor="heavyBlue" color="white" width="100vw" minHeight="100vh">
         <Component {...pageProps} />
       </Flex>
     </ChakraProvider>
