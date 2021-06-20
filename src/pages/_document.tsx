2cf07de226ad9ffe1afe0b24408ece1c85ee03b6
tomokiqi <tomokiqi@gmail.com>
2021-06-20 13:55:01 +0900

    for seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,11 @@ export default class Document extends NextDocument {
       <Html>
         <Head />
         <title>Tomokiqi's Portfolio</title>
+        <meta property="og:title" content="Tomokiqi's Portfolio" />
+        <meta
+          property="og:description"
+          content="This is the site which publishes my blogs and services."
+        />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <body>
           <Main />
