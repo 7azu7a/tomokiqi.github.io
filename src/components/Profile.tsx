72000d0e04ecd3ca73f6dc7721d5483d776ee03d
tomokiqi <tomokiqi@gmail.com>
2021-06-21 11:53:58 +0900

    for updating visibility

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,6 @@ import { FaTwitter, FaGithub } from "react-icons/fa";
 import {
   Flex,
   HStack,
-  Box,
   Heading,
   Text,
   VStack,
@@ -20,45 +19,37 @@ export const Profile = () => {
       <ChakraImage
         alt="author photo"
         src={"/me.png"}
-        width="8em"
-        height="8em"
+        width="10em"
+        height="10em"
         mx="2em"
         my="1em"
         objectFit="cover"
       />
       <VStack alignItems="flex-start" textAlign="justify">
-        <Box mb="1em">
-          <Heading
-            fontSize="1em"
-            mb="0.5em"
-            textAlign={{ base: "center", sm: "start" }}
-          >
-            西條 友喜
-          </Heading>
-          <Text fontSize="0.75em">
-            通信キャリアにて自社サービスのフロントエンド開発や政府研究開発のシステム設計に従事。
-            <br />
-            Scrum Alliance®︎ 認定スクラムマスター。趣味はイタリアン料理など。
-          </Text>
-        </Box>
-        <Box>
-          <Text mb="0.75em" fontSize="0.75em">
-            TypeScript ｜ React ｜ Next ｜ Express ｜ Scala ｜ Akka ｜ GitHub ｜
-            Scrum Master
-          </Text>
-          <HStack spacing="0.75em">
-            <Link href="https://twitter.com/tomokiqi">
-              <a>
-                <FaTwitter color="white" />
-              </a>
-            </Link>
-            <Link href="https://github.com/tomokiqi">
-              <a>
-                <FaGithub color="white" />
-              </a>
-            </Link>
-          </HStack>
-        </Box>
+        <Heading fontSize="1.25em" textAlign={{ base: "center", sm: "start" }}>
+          西條 友喜
+        </Heading>
+        <Text>
+          通信キャリアにて自社サービスのフロントエンド開発や政府研究開発のシステム設計に従事。
+          <br />
+          Scrum Alliance®︎ 認定スクラムマスター。趣味はイタリアン料理など。
+        </Text>
+        <Text mb="1em">
+          TypeScript ｜ React ｜ Next ｜ Express ｜ Scala ｜ Akka ｜ GitHub ｜
+          Scrum Master
+        </Text>
+        <HStack spacing="0.75em">
+          <Link href="https://github.com/tomokiqi">
+            <a>
+              <FaGithub color="white" />
+            </a>
+          </Link>
+          <Link href="https://twitter.com/tomokiqi">
+            <a>
+              <FaTwitter color="white" />
+            </a>
+          </Link>
+        </HStack>
       </VStack>
     </Flex>
   );
