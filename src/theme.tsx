8c0e3b6c2c1ebae31daa09b5fd65b4abe6de1d9e
tomokiqi <tomokiqi@gmail.com>
2021-07-15 00:11:37 +0900

    to format all files

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,24 +1,24 @@
-import { extendTheme, theme as chakraTheme } from "@chakra-ui/react";
-import { createBreakpoints } from "@chakra-ui/theme-tools";
+import { extendTheme, theme as chakraTheme } from '@chakra-ui/react';
+import { createBreakpoints } from '@chakra-ui/theme-tools';
 
 const fonts = {
   ...chakraTheme.fonts,
   heading:
-    "Georgia, 游明朝, Yu Mincho, YuMincho, Hiragino Mincho, ProN, HGS明朝E, メイリオ, Meiryo, serif",
-  body: "Georgia, 游明朝, Yu Mincho, YuMincho, Hiragino Mincho, ProN, HGS明朝E, メイリオ, Meiryo, serif",
+    'Georgia, 游明朝, Yu Mincho, YuMincho, Hiragino Mincho, ProN, HGS明朝E, メイリオ, Meiryo, serif',
+  body: 'Georgia, 游明朝, Yu Mincho, YuMincho, Hiragino Mincho, ProN, HGS明朝E, メイリオ, Meiryo, serif',
 };
 
 const breakpoints = createBreakpoints({
-  sm: "40em",
-  md: "52em",
-  lg: "64em",
-  xl: "80em",
+  sm: '40em',
+  md: '52em',
+  lg: '64em',
+  xl: '80em',
 });
 
 const theme = extendTheme({
   colors: {
-    black: "#16161D",
-    heavyBlue: "#1A202C",
+    black: '#16161D',
+    heavyBlue: '#1A202C',
   },
   fonts,
   breakpoints,
