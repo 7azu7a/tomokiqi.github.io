712d3c8dd3d422058e30202d6814a39178b58eb2
tomokiqi <tomokiqi@gmail.com>
2022-04-27 01:27:34 +0900

    "===== キー入力 ===== "方向キーの無効化 noremap <Up> <Nop> noremap <Down> <Nop> noremap <Left> <Nop> noremap <Right> <Nop> inoremap <Up> <Nop> inoremap <Down> <Nop> inoremap <Left> <Nop> inoremap <Right> <Nop>
    "入力モード時のカーソル移動
    inoremap <C-j> <Down>
    inoremap <C-k> <Up>
    inoremap <C-h> <Left>
    inoremap <C-l> <Right>
    
    "jキーを二度押しでESCキー
    inoremap <silent> jj <Esc>
    inoremap <silent> っj <ESC>
    
    "===== その他 =====
    "履歴を10000件保存
    set history=10000
    
    "カラースキーマの適用
    colorscheme molokai
    
    This reverts commit 18b466378e665e9c29897dad64ace4a1efa6c419.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,13 +10,14 @@ const globalStyle = css`
   }
   body {
     width: 100%;
-    min-height: 100vh;
+    height: 100vh;
     display: flex;
     justify-content: center;
     align-items: center;
     perspective: 1024px;
     transform-style: preserve-3d;
     position: relative;
+    overflow: hidden;
     color: #333;
     font-family: 'FuturaPT', sans-serif;
   }
